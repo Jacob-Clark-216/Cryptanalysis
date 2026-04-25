6ff63e6b196c23d2a58b32c60ee44444a4a3466a
Jacob Clark <216jakeclark@gmail.com>
2026-04-25 16:15:51 +0100

    Feat: add character count and frequency logic

--- main.py
+++ main.py
@@ -19,4 +19,37 @@ while content == None:
     except FileNotFoundError:
         print("Sorry, that file cannot be found. It may be missing or be in a differnt location. Please check the file location and spelling before trying again.")
 
-print(content)
+
+# Split the content into individual lines
+lines = content.splitlines()
+
+# Replace all newline characters with spaces and then split the content at any spaces to get a list of words
+words = content.replace("\n", " ").split(" ")
+
+# Record Characters
+chars = list(content.replace("\n", ""))
+unique_chars = set(content.replace("\n", " "))
+
+# Identify character counts
+char_counts = {}
+for c in unique_chars:
+    char_counts[c] = content.count(c)
+# Calculate character frequency
+char_frequency = {}
+for c in unique_chars:
+    char_frequency[c] = (char_counts[c]*100)/len(chars)
+
+# Provide Output
+output = "\n\nFile contains:\n"
+output += f"{len(lines)} lines\n"
+output += f"{len(words)} words\n"
+output += f"{len(chars)} non-whitespace characters\n\n"
+output += f"{len(unique_chars)} Unique Characters Found: {unique_chars}\n\n"
+output += f"Character stats:\n"
+for i in char_counts:
+    output += f"{i} | {char_counts[i]} | {char_frequency[i]:.2f}%\n"
+print(output)
+
+# print(f"{content}\n")
+# print(f"{lines}\n")
+# print(f"{words}\n")
